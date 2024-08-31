c2e18b1f0da5b184cdf8e9b78ddfadf8d9cd89ae
withdrw <finesse@usa.com>
2024-08-31 16:18:11 -0500

    debugging tailwind issues

--- Client/Ethrify/src/App.jsx
+++ Client/Ethrify/src/App.jsx
@@ -1,3 +1,12 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+import "./App.css";
+
+const App = () => {
+  return (
+    <h1 className="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  )
+
 }
+
+export default App;
