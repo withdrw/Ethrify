dea49096d805c39e166542bf5d5b36989d4070ff
opalak12 <palak@MacBook-Pro.attlocal.net>
2024-08-31 22:37:26 -0500

    navbar wip

--- Client/Ethrify/src/components/Navbar/Navbar.jsx
+++ Client/Ethrify/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,7 @@
 import { AiOutlineClose } from 'react-icons/ai';
+import { IoIosMenu } from "react-icons/io";
 import logo from '/logo.jpg';
+import { useState } from 'react';
 
 const NavbarItem = ({ title, classProps}) => {
     return (
@@ -10,6 +12,7 @@ const NavbarItem = ({ title, classProps}) => {
 }
 
 const Navbar = () => {
+    const [toggleMenu, setToggleMenu] = useState(false);
     return (
         <nav className='w-full flex md:justify-center justify-between'>
             <div className='md:flex-[0.5] justify-center '>
@@ -17,11 +20,25 @@ const Navbar = () => {
             </div>
             <ul>
                 {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index)=>(
-                    
                     <NavbarItem key={item+index} title={item} />
-
                 ))}
             </ul>
+            <div className="menu-container">
+                {!toggleMenu && (
+                    <IoIosMenu className="menu-icon" onClick={() => setToggleMenu(true)} />
+                )}
+                {toggleMenu && (
+                    <ul className="menu-list">
+                        <li className="menu-close-icon">
+                            <AiOutlineClose onClick={() => setToggleMenu(false)} />
+                        </li>
+                        {["Market", "Exchange", "Tutorials", "Wallets"].map(
+                            (item, index) => <NavBarItem key={item + index} title={item} classprops="menu-item" />,
+                        )}
+                    </ul>
+                )}
+            </div>
+
         </nav>
      )
 }
