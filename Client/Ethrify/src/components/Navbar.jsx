72ef71b13e3b133a92256f966cc7e493d9b22d26
withdrw <finesse@usa.com>
2024-08-31 20:12:40 -0500

    Start on welcome page

--- Client/Ethrify/src/components/Navbar.jsx
+++ Client/Ethrify/src/components/Navbar.jsx
@@ -1,19 +1,15 @@
-import { AiOutlineClose } from 'react-icons/ai';
-import logo from '../../public/logo.jpg';
+import logo from "../../public/logo.jpg";
+import './Navbar.css'
 
 const Navbar = () => {
-    return (
-        <nav className='w-full flex md:justify-center justify-between'>
-            <div className='justify-center '>
-                    <img src={logo} alt="logo">
-                    </img>
-                <h1>
-                    This is the navbar !
-                </h1>
-            </div>
-
-        </nav>
-     )
-}
+  return (
+    <nav className="w-full flex md:justify-center justify-between">
+      <div className="justify-center ">
+        <img className="w-36" src={logo} alt="logo"></img>
+        <h1 className="">This is the navbar !</h1>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
