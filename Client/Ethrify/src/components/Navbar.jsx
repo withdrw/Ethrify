4a0f96c855e7ba4f012fa3da72afc20a454c230d
opalak12 <palak@MacBook-Pro.attlocal.net>
2024-08-31 21:44:05 -0500

    adding navbar components

--- Client/Ethrify/src/components/Navbar.jsx
+++ Client/Ethrify/src/components/Navbar.jsx
@@ -1,20 +1,27 @@
 import { AiOutlineClose } from 'react-icons/ai';
-import logo from '../../public/logo.jpg';
+import logo from '/logo.jpg';
+
+const NavbarItem = ({ title, classProps}) => {
+    return (
+        <li className={`${classProps}`} >
+            {title}
+        </li>
+    )
+}
 
 const Navbar = () => {
     return (
         <nav className='w-full flex md:justify-center justify-between'>
-            <div className='justify-center '>
-                <AiOutlineClose>
-                    
-                </AiOutlineClose>
-                    <img src={logo} alt="logo">
-                    </img>
-                <h1>
-                    This is the navbar !
-                </h1>
+            <div className='md:flex-[0.5] justify-center '>
+                <img className='logo' src={logo} alt="logo"/>
             </div>
+            <ul>
+                {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index)=>(
+                    
+                    <NavbarItem key={item+index} title={item} />
 
+                ))}
+            </ul>
         </nav>
      )
 }
